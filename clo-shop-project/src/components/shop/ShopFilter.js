8dc82bba55fe874a4dbf1de01fd0fafdf5d1e168
Mehran <info.mehranazizi@yahoo.com>
2022-04-23 21:19:52 -0700

    Shop page : debug

--- clo-shop-project/src/components/shop/ShopFilter.js
+++ clo-shop-project/src/components/shop/ShopFilter.js
@@ -1,4 +1,4 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 
 //Styles
 import styles from "./ShopFilter.module.css";
@@ -7,72 +7,80 @@ import styles from "./ShopFilter.module.css";
 import search from "../../assets/img/search.svg";
 
 const ShopFilter = ({ filtered, filterHandler }) => {
-  const [filterMenu , setFilterMenu] = useState(false);
+  const [filterMenu, setFilterMenu] = useState(false);
 
   const filterMenuHandler = () => {
     setFilterMenu(!filterMenu);
-  }
+  };
 
   return (
     <>
-    <div className={filterMenu ? styles.filterOpen : styles.filter}>
-      <div className={styles.field}>
-        <h4>جستجو در محصولات</h4>
-        <div className={styles.searchBox}>
-          <input
-            type="text"
-            className={styles.search}
-            name="search"
-            placeholder="نام محصول مورد نظر"
-            value={filtered.search}
-            onChange={filterHandler}
-          />
-          <img src={search} alt="search" className={styles.searchIcon} />
+      <div className={filterMenu ? styles.filterOpen : styles.filter}>
+        <div className={styles.field}>
+          <h4>جستجو در محصولات</h4>
+          <div className={styles.searchBox}>
+            <input
+              type="text"
+              className={styles.search}
+              name="search"
+              placeholder="نام محصول مورد نظر"
+              value={filtered.search}
+              onChange={filterHandler}
+            />
+            <img src={search} alt="search" className={styles.searchIcon} />
+          </div>
+        </div>
+        <div className={styles.field}>
+          <h4>دسته بندی محصولات</h4>
+          <div className={styles.category}>
+            <ul>
+              <li id="category مردانه" onClick={filterHandler}>
+                مردانه
+              </li>
+              <li id="category زنانه" onClick={filterHandler}>
+                زنانه
+              </li>
+              <li id="category بچگانه" onClick={filterHandler}>
+                بچگانه
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.field}>
+          <h4>قیمت</h4>
+          <div className={styles.priceBox}>
+            <small className={styles.pricerange}>حداقل قیمت</small>
+            <input
+              type="number"
+              name="fromPrice"
+              onChange={filterHandler}
+              value={filtered.fromPrice}
+            />
+            <small className={styles.unitPrice}>تومان</small>
+          </div>
+          <div className={styles.priceBox}>
+            <small className={styles.pricerange}>حداکثر قیمت</small>
+            <input
+              type="number"
+              name="toPrice"
+              onChange={filterHandler}
+              value={filtered.toPrice}
+            />
+            <small className={styles.unitPrice}>تومان</small>
+          </div>
         </div>
       </div>
-      <div className={styles.field}>
-        <h4>دسته بندی محصولات</h4>
-        <div className={styles.category}>
-          <ul>
-            <li id="category مردانه" onClick={filterHandler}>
-              مردانه
-            </li>
-            <li id="category زنانه" onClick={filterHandler}>
-              زنانه
-            </li>
-            <li id="category بچگانه" onClick={filterHandler}>
-              بچگانه
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className={styles.field}>
-        <h4>قیمت</h4>
-        <div className={styles.priceBox}>
-          <small className={styles.pricerange}>حداقل قیمت</small>
-          <input
-            type="number"
-            name="fromPrice"
-            onChange={filterHandler}
-            value={filtered.fromPrice}
-          />
-          <small className={styles.unitPrice}>تومان</small>
-        </div>
-        <div className={styles.priceBox}>
-          <small className={styles.pricerange}>حداکثر قیمت</small>
-          <input
-            type="number"
-            name="toPrice"
-            onChange={filterHandler}
-            value={filtered.toPrice}
-          />
-          <small className={styles.unitPrice}>تومان</small>
-        </div>
-      </div>
-    </div>
-      <div className={styles.buttonFilter} onClick={filterMenuHandler}>
-        فیلتر
-        <i className="fas fa-filter"></i>
+      <div className={filterMenu ? styles.closeFilter : styles.buttonFilter} onClick={filterMenuHandler}>
+        {filterMenu ? (
+        <div>
+          
+          <i className="fas fa-times"></i>
+        </div>) : (
+        <div>
+          فیلتر
+          <i className="fas fa-filter"></i>
+        </div>)}
+        
       </div>
     </>
   );
